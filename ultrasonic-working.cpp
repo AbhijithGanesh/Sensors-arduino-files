82919988448fd22cfc33c65558ba297135fa4296
AbhijithGanesh <git.watch.Abhijith@outlook.com>
2023-04-10 10:33:10 +0530

    Addition of test codes
    Signed-off-by: AbhijithGanesh <git.watch.Abhijith@outlook.com>

---
+++ ultrasonic-working.cpp
@@ -0,0 +1,24 @@
+#define trigPin 8
+#define echoPin 9
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+    long duration, distance;
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration / 58.2;
+    Serial.print(distance);
+    Serial.println("cm");
+    delayMicroseconds(100);
+}
